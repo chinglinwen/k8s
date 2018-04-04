e0740e71257563daee5f2fe5b4c5d64e54e07961
wenzhenglin <wenzhenglin@qianbao.com>
2018-04-04 10:00:33 +0800

    Add httpload for testing.

---
+++ testing/httpload/fstest-short.sh
@@ -0,0 +1,8 @@
+. funcs
+
+makebase "data-$( date +%F_%H%M%S )"
+
+fs 1s 100
+fs 10s 100
+fs 10s 1000
+fs 10s 10000
---
+++ testing/httpload/fstest.sh
@@ -0,0 +1,13 @@
+. funcs
+
+makebase "data-$( date +%F_%H%M%S )"
+
+fs 1s 100
+fs 10s 100
+fs 10s 1000
+fs 10s 10000
+
+fs 1m 100
+fs 10m 100
+fs 10m 1000
+fs 10m 10000
