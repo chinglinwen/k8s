097d7e27a3c9732ca0d4c243bcf74cdc31ab7495
wenzhenglin <wenzhenglin@qianbao.com>
2018-05-08 18:46:33 +0800

    netperf: change time print.

--- testing/netperf/start.sh
+++ testing/netperf/start.sh
@@ -1,4 +1,4 @@
-echo "Start time: $(date "+%s%N")" >> netperf.log
+echo "Start time: $( date +%F_%T )" >> netperf.log
 netperf -kubeConfig ~/.kube/config &>> netperf.log &
 tail -f netperf.log
-echo "End time(manual): $(date "+%s%N")" >> netperf.log
+echo "End time(manual): $( date +%F_%T )" >> netperf.log
