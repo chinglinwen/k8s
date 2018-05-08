ef2df3feed283879b643f7e4e81cffad8cdbfec7
wenzhenglin <wenzhenglin@qianbao.com>
2018-05-08 18:04:32 +0800

    netperf: fix start time for netperf.

--- testing/netperf/start.sh
+++ testing/netperf/start.sh
@@ -1,4 +1,4 @@
 echo "Start time: $(date "+%s%N")" >> netperf.log
-netperf -kubeConfig ~/.kube/config &> netperf.log &
+netperf -kubeConfig ~/.kube/config &>> netperf.log &
 tail -f netperf.log
-
+echo "End time(manual): $(date "+%s%N")" >> netperf.log
