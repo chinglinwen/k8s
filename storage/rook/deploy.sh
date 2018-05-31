be55e271765c578afe14179f9ea90b6c758498f8
wenzhenglin <wenzhenglin@qianbao.com>
2018-05-31 16:49:36 +0800

    storage: add rook-filesystem.

--- storage/rook/deploy.sh
+++ storage/rook/deploy.sh
@@ -1,4 +1,5 @@
 kubectl create -f rook-operator.yaml
 kubectl create -f rook-cluster.yaml
 kubectl create -f rook-tools.yaml
-kubectl apply -f rook-storageclass.yaml
+kubectl create -f rook-storageclass.yaml
+kubectl create -f rook-filesystem.yaml
