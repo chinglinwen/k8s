75e28969396bece20b4e506c270fe200f7626fe2
wenzhenglin <wenzhenglin@qianbao.com>
2018-06-07 13:51:18 +0800

    testing: dns test auto get dns service ip.

--- testing/dns-perf-tests/local-perf/run.sh
+++ testing/dns-perf-tests/local-perf/run.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-if [ "x$1" = "x" ]; then
+if [ "x$1" = "x-h" ]; then
   echo "Usage: $0 dns-ip-addr"
   exit 1
 fi
-dns=${1:=10.96.0.10}
+dnsip="$( kubectl get svc -o wide -n kube-system | grep dns | awk '{ print $3 }' )"
+if [ "x$1" != "x" ]; then
+  dns=$1
+else
+  dns=$dnsip
+fi
 ./bin/queryperf-linux_amd64 -d domain.lst.queryperf -l 30 -s $dns -p 53
