008ff1e937d64b9af36c86d02f013a42fd8fcb38
wenzhenglin <wenzhenglin@qianbao.com>
2018-06-07 13:54:25 +0800

    testing: dns test auto get dns service ip (adapt for v1.6 too ).

--- testing/dns-perf-tests/local-perf/run.sh
+++ testing/dns-perf-tests/local-perf/run.sh
@@ -3,7 +3,7 @@ if [ "x$1" = "x-h" ]; then
   echo "Usage: $0 dns-ip-addr"
   exit 1
 fi
-dnsip="$( kubectl get svc -o wide -n kube-system | grep dns | awk '{ print $3 }' )"
+dnsip="$( kubectl get svc kube-dns -n kube-system -o yaml |grep 'IP:' | awk '{ print $2 }' )"
 if [ "x$1" != "x" ]; then
   dns=$1
 else
