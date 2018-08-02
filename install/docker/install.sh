a3197717db3b02d90b470c6f3064948bd8ff9f6a
wenzhenglin <wenzhenglin@qianbao.com>
2018-08-02 19:27:30 +0800

    Update 'install/docker/install.sh'
    update insecure registry.

--- install/docker/install.sh
+++ install/docker/install.sh
@@ -14,7 +14,7 @@ wget -O /etc/yum.repos.d/docker-ce.cn.repo http://fs.qianbao-inc.com/k8s/docker/
 
 yum install docker-ce -y
 
-sed -i 's_ExecStart=/usr/bin/dockerd.*_ExecStart=/usr/bin/dockerd \\\n  --data-root=/home/data/docker \\\n  --registry-mirror=https://registry.docker-cn.com \\\n  --insecure-registry=reg.qianbao-inc.com_' /usr/lib/systemd/system/docker.service
+sed -i 's_ExecStart=/usr/bin/dockerd.*_ExecStart=/usr/bin/dockerd \\\n  --data-root=/home/data/docker \\\n  --registry-mirror=https://registry.docker-cn.com \\\n  --insecure-registry=harbor.wk.qianbao-inc.com --insecure-registry harbor-test.wk.qianbao-inc.com --insecure-registry reg.qianbao-inc.com_' /usr/lib/systemd/system/docker.service
 
 systemctl daemon-reload
 systemctl restart docker
