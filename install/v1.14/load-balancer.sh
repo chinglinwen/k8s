5d6a41cfd69576984b7b1e66996454c70393f2f4
wenzhenglin <wenzhenglin@haodai.net>
2019-04-15 18:44:30 +0800

    add load balancer info and notes

--- install/v1.14/load-balancer.sh
+++ install/v1.14/load-balancer.sh
@@ -1,33 +1,33 @@
 #!/bin/sh
 # install ipvs on all master nodes
 
-export PRIVATE_IP="$( ip addr show `ip r |grep default | awk '{ print $5 }'` | \
-                     grep link -A 1 | grep -Po 'inet \K[\d.]+' )"
+# test, keepalived will create ipvs entry
 
-ipvsadm -A -t $PRIVATE_IP:9443 -s wlc
-ipvsadm -a -t $PRIVATE_IP:9443 -r 172.31.90.100:6443 -m
-ipvsadm -a -t $PRIVATE_IP:9443 -r 172.31.90.101:6443 -m
-ipvsadm -a -t $PRIVATE_IP:9443 -r 172.31.90.102:6443 -m
+# export PRIVATE_IP="$( ip addr show `ip r |grep default | awk '{ print $5 }'` | \
+#                      grep link -A 1 | grep -Po 'inet \K[\d.]+' )"
 
-ipvsadm -l
-ipvsadm-save -n > /etc/sysconfig/ipvsadm
+# ipvsadm -A -t $PRIVATE_IP:9443 -s wlc
+# ipvsadm -a -t $PRIVATE_IP:9443 -r 172.31.90.100:6443 -m
+# ipvsadm -a -t $PRIVATE_IP:9443 -r 172.31.90.101:6443 -m
+# ipvsadm -a -t $PRIVATE_IP:9443 -r 172.31.90.102:6443 -m
+
+# ipvsadm -l
+# ipvsadm-save -n > /etc/sysconfig/ipvsadm
 
 # testing
-wget http://fs.devops.haodai.net/soft/fileserver2 -O /usr/local/bin/fs && chmod +x /usr/local/bin/fs
+# wget http://fs.devops.haodai.net/soft/fileserver2 -O /usr/local/bin/fs && chmod +x /usr/local/bin/fs
 
-curl 172.31.90.219:9443
+# curl 172.31.90.219:9443
 
+# export PRIVATE_IP="$( ip addr show `ip r |grep default | awk '{ print $5 }'` | \
+#                      grep link -A 1 | grep -Po 'inet \K[\d.]+' )"
 
-
-export PRIVATE_IP="$( ip addr show `ip r |grep default | awk '{ print $5 }'` | \
-                     grep link -A 1 | grep -Po 'inet \K[\d.]+' )"
-
-ipvsadm -A -t $PRIVATE_IP:9444 -s wlc
-ipvsadm -a -t $PRIVATE_IP:9444 -r 172.31.90.100:6443 -m
+# ipvsadm -A -t $PRIVATE_IP:9444 -s wlc
+# ipvsadm -a -t $PRIVATE_IP:9444 -r 172.31.90.100:6443 -m
 
 
-ipvsadm -A -t 172.31.90.219:9444 -s wlc
-ipvsadm -a -t 172.31.90.219:9444 -r 172.31.90.100:6443 -m
+# ipvsadm -A -t 172.31.90.219:9444 -s wlc
+# ipvsadm -a -t 172.31.90.219:9444 -r 172.31.90.100:6443 -m
 
 
 
@@ -35,7 +35,8 @@ echo "net.ipv4.conf.eth1.arp_ignore = 1" >> /etc/sysctl.conf
 echo "net.ipv4.conf.eth1.arp_announce = 2" >> /etc/sysctl.conf
 sysctl -p
 
-
+# keepalived dr mode need this
+cat >>/etc/rc.local<<eof
 /sbin/ifconfig lo down
 /sbin/ifconfig lo up
 echo 1 > /proc/sys/net/ipv4/conf/lo/arp_ignore
@@ -46,4 +47,4 @@ echo 2 > /proc/sys/net/ipv4/conf/all/arp_announce
 ip route add 172.31.90.219 via 0.0.0.0 dev lo:0
 /bin/systemctl stop firewalld.service
 setenforce 0
-
+eof
